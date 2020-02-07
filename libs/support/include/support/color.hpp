2e696f8236b9810448b920b2c17ae87415937572
Danny Bøgsted Poulsen <dannybpoulsen@hotmail.com>
2020-02-07 11:13:41 +0100

    Moved the loader in as subdirectory of model

--- libs/support/include/support/color.hpp
+++ libs/support/include/support/color.hpp
@@ -4,23 +4,23 @@
 namespace MiniMC {
   namespace Support {
   class Color {
-	public:
-	  virtual std::string toString () = 0;
-	};
-
-	class HSLColor : public Color {
-	public:
-	  HSLColor (double h,double s, double l) : h(h),s(s),l(l) {}
-	  virtual std::string toString () {
-		std::stringstream str;
-		str << h <<" " << s <<" "<< l;
-		return str.str ();
-	  }
-	private:
-	  double h;
-	  double s;
-	  double l;
-	};
+  public:
+    virtual std::string toString () = 0;
+  };
+    
+    class HSLColor : public Color {
+    public:
+      HSLColor (double h,double s, double l) : h(h),s(s),l(l) {}
+      virtual std::string toString () {
+	std::stringstream str;
+	str << h <<" " << s <<" "<< l;
+	return str.str ();
+      }
+    private:
+      double h;
+      double s;
+      double l;
+    };
   }
   
 }
