e39cb955146bea5d14ba5a590f989c4e87b9b645
Danny Bøgsted Poulsen <dannybpoulsen@hotmail.com>
2020-03-04 09:36:29 +0100

    Missing file

---
+++ libs/support/include/support/workinglist.hpp
@@ -0,0 +1,25 @@
+#ifndef _WORKING_LIST__
+#define _WORKING_LIST__
+
+#include <vector>
+
+namespace MiniMC {
+  namespace Support {
+    template<class Element,class Container = std::vector<Element>>
+    class WorkingList {
+    public:
+      auto inserter () {return std::back_inserter (container);}
+      auto begin () {return container.begin();}
+      auto end () {return container.end();}
+      auto size () const {return container.size ();}
+    private:
+      Container container;
+    };
+    
+    
+  }
+}
+
+
+
+#endif
