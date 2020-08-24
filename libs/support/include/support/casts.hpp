2ec0fd8360864fc9b0d17aa125cf84aac51310b7
Danny Bøgsted Poulsen <dannybpoulsen@hotmail.com>
2020-08-24 19:27:26 +0200

    Sign-extending

--- libs/support/include/support/casts.hpp
+++ libs/support/include/support/casts.hpp
@@ -2,6 +2,8 @@
 #define _CASTS__
 
 #include "support/types.hpp"
+#include "support/exceptions.hpp"
+
 
 namespace MiniMC {
   namespace Support {
@@ -18,15 +20,7 @@ namespace MiniMC {
     }
 
     template<class T, class P>
-    P sext (const T& t) {
-      static_assert(sizeof(P) >= sizeof(T));
-      using uns_from = typename EquivSigned<T>::type;
-      using uns_to = typename EquivSigned<P>::type;
-      
-      uns_from casted = bit_cast<T,uns_from> (t);
-      uns_to res = static_cast<uns_to> (casted);
-      return bit_cast<uns_to,P> (res);
-    }
+    P sext (const T t);
 
     template<class T, class P>
     P trunc (const T& t) {
