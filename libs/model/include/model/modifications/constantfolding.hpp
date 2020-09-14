469019a55f878038bbe0fa5671a6d3d7032da8fb
Danny Bøgsted Poulsen <dannybpoulsen@hotmail.com>
2020-09-14 12:48:19 +0200

    One more missing file

---
+++ libs/model/include/model/modifications/constantfolding.hpp
@@ -0,0 +1,28 @@
+#ifndef _CONSTANT_FOLDING__
+#define _CONSTANT_FOLDING__
+
+#include "model/cfg.hpp"
+#include "support/sequencer.hpp"
+
+
+namespace MiniMC  {
+  namespace Model {
+	namespace Modifications {
+	  void foldConstants (Function& f);
+	  
+	  struct FoldConstants : public MiniMC::Support::Sink<MiniMC::Model::Program> {
+		virtual bool run (MiniMC::Model::Program&  prgm) {
+		  for (auto& F : prgm.getFunctions ()) {
+			foldConstants (*F);
+			
+		  }
+		  return true;
+		}
+	  };
+	}
+	
+  }
+}
+
+
+#endif
