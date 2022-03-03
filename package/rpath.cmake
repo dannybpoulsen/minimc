916bc3bb4e97932729e4521e482fb8d0ca3bc75f
Danny Bøgsted Poulsen <dannybpoulsen@hotmail.com>
2022-03-03 21:21:55 +0100

    fsdfhk

---
+++ package/rpath.cmake
@@ -0,0 +1,9 @@
+# use, i.e. don't skip the full RPATH for the build tree
+SET(CMAKE_SKIP_BUILD_RPATH  FALSE)
+
+# when building, don't use the install RPATH already
+# (but later on when installing)
+SET(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE) 
+
+set(CMAKE_INSTALL_RPATH "$ORIGIN:$ORIGIN/../lib")
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
