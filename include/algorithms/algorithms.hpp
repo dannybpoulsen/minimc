67b3e11c35149d0777bb796957a6abb92100283a
Danny Bøgsted Poulsen <dannybpoulsen@hotmail.com>
2022-04-21 18:43:43 +0200

    Missing file

---
+++ include/algorithms/algorithms.hpp
@@ -0,0 +1,31 @@
+#ifndef _ALGORITHMS__
+#define _ALGORITHMS__
+
+#include <list>
+#include <functional>
+
+namespace MiniMC {
+  namespace Algorithms {
+    template<class T>
+    struct Observable {
+      using observer = std::function<void(const T&)>;
+      void operator= (T&& t) {
+	val = t; 
+      }
+
+      void notif_listeners () const {
+	for ( auto& o : observers)
+	  o (val);
+      }
+      
+      void listen (observer obs) {observers.push_back (obs);}
+    private:
+      T val;
+      std::list<observer> observers;
+    };
+
+  }
+}
+
+
+#endif
