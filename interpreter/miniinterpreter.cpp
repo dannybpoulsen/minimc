aeb435a48d0ce370968c7710c8df686a1dc77198
larsbpf <lbpf@cs.aau.dk>
2022-09-13 12:14:21 +0200

    Added functionality for look ahead

--- interpreter/miniinterpreter.cpp
+++ interpreter/miniinterpreter.cpp
@@ -11,13 +11,17 @@
 
 namespace po = boost::program_options;
 
-MiniMC::Model::Edge *promptForEdge(MiniMC::CPA::AnalysisState state);
+MiniMC::Model::Edge *promptForEdge(MiniMC::CPA::AnalysisState state, int lookahead, std::ostream& os);
+void traverseCFG(MiniMC::CPA::AnalysisState initialstate, MiniMC::CPA::AnalysisTransfer transferer, int lookahead, std::ostream& os);
+void doLookAhead(MiniMC::Model::Edge* edge, int lookahead, std::ostream& os);
+
 
 int main(int argc, char *argv[]) {
 
   // Load program from file
   std::string inputname;
   std::string task("main");
+  int lookahead = 1;
   if (argc) {
     inputname = argv[1];
   }
@@ -47,51 +51,49 @@ int main(int argc, char *argv[]) {
       MiniMC::CPA::InitialiseDescr{prgm.getEntryPoints(), prgm.getHeapLayout(),
                                    prgm.getInitialiser(), prgm});
 
+
+  traverseCFG(initialstate,transferer, lookahead, std::cout);
+
+}
+
+void traverseCFG(MiniMC::CPA::AnalysisState initialstate,
+                  MiniMC::CPA::AnalysisTransfer transferer, int lookahead,
+                  std::ostream &os) {
   std::list<MiniMC::CPA::AnalysisState> waiting;
-
   MiniMC::proc_t proc{0};
-
   waiting.push_back(initialstate);
 
-  // Step through the CFG
-  while (waiting.size ()) {
-
+  while(waiting.size ()){
+    MiniMC::CPA::AnalysisState newstate;
     auto state = std::move(waiting.back());
     waiting.pop_back();
 
-    std::cout << state;
 
-    MiniMC::CPA::AnalysisState newstate;
-
-    // Choose edge
-    MiniMC::Model::Edge* edge = promptForEdge(state);
-
-    // Process chosen edge
-    if (transferer.Transfer(state, edge, proc, newstate)) {
+    os << state;
+    MiniMC::Model::Edge* edge = promptForEdge(state,lookahead,os);
+    if(transferer.Transfer(state,edge,proc,newstate)) {
       waiting.push_front(newstate);
     }
   }
-
-  std::cout << "Reached the end";
 }
 
-MiniMC::Model::Edge *promptForEdge(MiniMC::CPA::AnalysisState state) {
+MiniMC::Model::Edge *promptForEdge(MiniMC::CPA::AnalysisState state, int lookahead, std::ostream& os) {
   int index = -1;
   int n = 0;
 
   std::vector<MiniMC::Model::Edge*> edges;
 
-  std::cout << "Folowing edges can be picked" << std::endl ;
   MiniMC::Algorithms::EdgeEnumerator enumerator{state};
   MiniMC::Algorithms::EnumResult res;
 
+  os << "Folowing edges can be picked" << std::endl ;
+
   // Print outgoing edges
   while (enumerator.getNext(res)) {
-    n++;
-    std::cout << n <<". ";
-    std::cout << *res.edge;
-    std::cout << std::endl;
     edges.push_back(res.edge);
+    n++;
+    os << n << ". "<< std::endl;
+    doLookAhead(res.edge, lookahead, os);
   }
 
   // Choose edge by index
@@ -102,4 +104,15 @@ MiniMC::Model::Edge *promptForEdge(MiniMC::CPA::AnalysisState state) {
   return edges[index-1];
 
 };
+
+void doLookAhead(MiniMC::Model::Edge* edge, int lookahead, std::ostream& os){
+  if(lookahead == 0){
+    return;
+  }
+  os << *edge;
+
+  for (auto it = edge->getTo()->ebegin(); it !=  edge->getTo()->eend(); ++it) {
+    doLookAhead(*it, lookahead-1,os);
+  }
+}
 
