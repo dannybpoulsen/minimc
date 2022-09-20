0fae6493374010244691b80bb932f23cc51c036a
larsbpf <lbpf@cs.aau.dk>
2022-09-20 08:23:58 +0200

    Working CLI

--- include/interpreter/task.hpp
+++ include/interpreter/task.hpp
@@ -8,14 +8,54 @@ namespace Interpreter {
 
 class Task {
 public:
-  virtual ~Task();
-  virtual MiniMC::CPA::AnalysisState* performTask() = 0;
+  virtual MiniMC::CPA::AnalysisState performTask() = 0;
+};
+
+class PrintStateTask : public Task {
+public:
+  PrintStateTask(MiniMC::CPA::AnalysisState state) : state(state){};
+  CPA::AnalysisState performTask() {
+    std::cout << this->state;
+    return state;
+  }
+
+private:
+  MiniMC::CPA::AnalysisState state;
+};
+
+class SingleStepTask : public Task {
+public:
+  SingleStepTask(CPA::AnalysisState state, Model::Edge *edge,
+                 const CPA::AnalysisTransfer &transfer)
+      : state(state), edge(edge), transfer(transfer) {}
+  CPA::AnalysisState performTask() override {
+    CPA::AnalysisState newstate;
+    MiniMC::proc_t proc{0};
+
+    if(transfer.Transfer(state, edge, proc, newstate)){
+      return newstate;
+    };
+
+    return state;
+  }
+
+private:
+  CPA::AnalysisState state;
+  Model::Edge *edge;
+  CPA::AnalysisTransfer transfer;
 };
 
 class TaskFactory {
 public:
-  virtual ~TaskFactory();
-  virtual Task* createTask(std::string,MiniMC::CPA::AnalysisState*) = 0;
+  virtual Task* createTask(std::string,std::unordered_map<std::string, CPA::AnalysisState> statemap,MiniMC::CPA::AnalysisTransfer) = 0;
+};
+
+class InterpreterTaskFactory : public TaskFactory{
+public:
+  Task *createTask(std::string string,
+                   std::unordered_map<std::string,CPA::AnalysisState> statemap, MiniMC::CPA::AnalysisTransfer transfer) override;
+private:
+  Model::Edge *promptForEdge(CPA::AnalysisState state);
 };
 
 } // namespace Interpreter
--- libs/interpreter/task.cpp
+++ libs/interpreter/task.cpp
@@ -6,28 +6,55 @@
 
 namespace MiniMC {
 namespace Interpreter {
-class PrintStateTask : public Task {
-  public:
-    PrintStateTask(MiniMC::CPA::AnalysisState* state ){this->state =state;};
-    MiniMC::CPA::AnalysisState* performTask(){
-      std::cout << this->state;
-      delete this;
-      return state;
-    }
-  private:
-    MiniMC::CPA::AnalysisState* state;
+Task *InterpreterTaskFactory::createTask(
+    std::string s, std::unordered_map<std::string, CPA::AnalysisState> statemap,
+    CPA::AnalysisTransfer transfer) {
+  if (s == "printState" || s == "p") {
+    return new PrintStateTask(statemap["current"]);
+  } else if (s == "step" || s == "s") {
+    return new SingleStepTask(statemap["current"],
+                              promptForEdge(statemap["current"]), transfer);
+  } else if () {
 
+  } else {
+    return nullptr;
+  }
+}
+
+Model::Edge *InterpreterTaskFactory::promptForEdge(CPA::AnalysisState state) {
+  int index = -1;
+  int n = 0;
+
+  std::vector<Model::Edge *> edges;
+
+  Algorithms::EdgeEnumerator enumerator{state};
+  Algorithms::EnumResult res;
+
+  std::cout << "Folowing edges can be picked" << std::endl;
+
+  // Print outgoing edges
+  while (enumerator.getNext(res)) {
+    edges.push_back(res.edge);
+    n++;
+    std::cout << n << ". " << std::endl;
+  }
+
+  if (edges.size() == 0) {
+    return nullptr;
+  }
+
+  // Choose edge by index
+  while (index <= 0) {
+    std::cin >> index;
+    if (edges[index - 1]) {
+      return edges[index - 1];
+    }
+    std::cout
+        << "The chosen index is not possible, please choose a index under "
+        << edges.size();
+  }
+  return nullptr;
 };
 
-  class InterpreterTaskFactory : public TaskFactory {
-    public:
-      Task* createTask(std::string s,MiniMC::CPA::AnalysisState* state){
-        if(s == "printState" && "p"){
-          return new PrintStateTask(state);
-        }
-        return nullptr;
-      }
-  };
-
-} // namespace Interpreter
+}// namespace Interpreter
 } // namespace MiniMC
