b1e821d1c99a871114b7597b71579231c793f782
larsbpf <lbpf@cs.aau.dk>
2022-09-28 09:19:48 +0200

    Folder structure for minimc-parser

---
+++ libs/loaders/mmc/loader.cpp
@@ -0,0 +1,6 @@
+namespace MiniMC {
+namespace Loader {
+
+
+}
+}
---
+++ libs/loaders/mmc/parser.hpp
@@ -0,0 +1,14 @@
+#ifndef MINIMC_PARSER_HPP
+#define MINIMC_PARSER_HPP
+
+namespace MiniMC {
+namespace Loader {
+
+class Parser {
+
+};
+
+}
+}
+
+#endif // MINIMC_PARSER_HPP
