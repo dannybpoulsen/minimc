16e845f1eda5004154cb28c73f89717bb285faf2
Danny Bøgsted Poulsen <dannybpoulsen@hotmail.com>
2022-10-15 14:37:29 +0200

    - Major rework of repository... - Libraries now have their cmake project and evertyhing related (e.g. source files, include files, tests ) to it should be confined in that project

--- tests/code/support/output.cpp
+++
@@ -1,9 +0,0 @@
-#include "support/feedback.hpp"
-
-int main () {
-  auto& messager =  MiniMC::Support::getMessager ();//makeMessager (MiniMC::Support::MessagerType::Terminal);
-  messager.message ("Hej");
-  messager.error ("Error");
-  messager.warning ("Warning");
-}
-  
