a6f3a563fe0f1c262302613e424c357f81599fb3
Danny Bøgsted Poulsen <dannybpoulsen@cs.aau.dk>
2024-12-20 09:33:11 +0100

    Added missing file (binary for producing symbols in loaded programs)

---
+++ bin/symbolc.cpp
@@ -0,0 +1,46 @@
+#include <boost/program_options.hpp>
+#include "minimc/model/output.hpp"
+#include "minimc/host/host.hpp"
+
+
+#include "plugin.hpp"
+
+#include <sstream>
+#include <format>
+
+namespace po = boost::program_options;
+
+
+std::string symbol_descr_text (const MiniMC::Model::Symbol& s) {
+  std::stringstream str;
+
+  std::visit (MiniMC::Support::Overload {
+      [&str](const MiniMC::Model::Register_wptr&) {str << "Register";},
+      [&str](const MiniMC::Model::Function_wptr&) {str << "Function";},
+      [&str](const MiniMC::Model::Location_wptr&) {str << "Location";},
+      [&str](const MiniMC::Model::HeapBlock_wptr&) {str << "HeapObject";},
+      [&str](const std::monostate& ) {str << "??";}
+	}
+    ,s.getUserData());
+  return str.str();
+}
+
+
+struct ConvCCommand : public Command{
+  MiniMC::Host::ExitCodes runCommand (MiniMC::Model::Program&& prgm, MiniMC::Support::Messager&,const SetupOptions&)  {
+
+    for (auto s : prgm.getRootFrame().symbols () ) {
+      std::cerr << std::format ("{:20} {:20} ", s.getFullName(), symbol_descr_text(s)) << std::endl;
+    }
+    
+    return MiniMC::Host::ExitCodes::AllGood;
+  }
+
+  std::string getName () const override {return "symbols";}
+  std::string getDescritpion () const override {return "Dump symbols of the program";}
+  
+  
+};
+
+static CommandRegistrar<ConvCCommand> symbols_reg;
+
