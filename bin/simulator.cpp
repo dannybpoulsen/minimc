c6e7df58f7bcc1d529e2dc9481a02388da1b63e7
Danny Bøgsted Poulsen <dannybpoulsen@cs.aau.dk>
2025-03-23 19:48:26 +0100

    fdjsdlf

---
+++ bin/simulator.cpp
@@ -0,0 +1,41 @@
+#include <boost/program_options.hpp>
+#include "minimc/model/output.hpp"
+#include "minimc/host/host.hpp"
+#include "minimc/cpa/interface.hpp"
+#include "minimc/simulator/simulator.hpp"
+#include "minimc/io/ostream.hpp"
+
+
+
+#include "plugin.hpp"
+
+namespace po = boost::program_options;
+
+
+
+struct CLICommand : public Command{
+  MiniMC::Host::ExitCodes runCommand (MiniMC::Model::Program&& prgm, MiniMC::Support::Messager&,const SetupOptions&)  {
+    auto cpa = MiniMC::CPA::makeCPA<MiniMC::CPA::CPAType::Concrete> ();
+    MiniMC::Simulator::Simulator simulator {cpa,&prgm};
+    MiniMC::IO::StreamPrompter prompter {std::cin,MiniMC::IO::os_ostream::out()};
+    MiniMC::Simulator::CommandParser cmdparser {MiniMC::IO::os_ostream::out(),prompter,&prgm};
+      
+    std::unique_ptr<MiniMC::Simulator::Command> command;
+    do {
+      command = cmdparser.parse ();
+      if(command)
+	command ->execute(&simulator);
+    }while(command);
+    
+    
+    return MiniMC::Host::ExitCodes::AllGood;
+  }
+
+  std::string getName () const override {return "cli";}
+  std::string getDescritpion () const override {return "Simple CLI interface to MiniMC";}
+  
+  
+};
+
+static CommandRegistrar<CLICommand> cli_reg;
+  
