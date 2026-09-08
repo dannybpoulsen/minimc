8186c8b041b0704ef4f470d8d306f6a04c48f717
Danny Bøgsted Poulsen <dannybpoulsen@hotmail.com>
2026-09-08 11:28:03 +0200

    Updated build

--- dependencies/doctest.cmake
+++ dependencies/doctest.cmake
@@ -4,7 +4,7 @@ FetchContent_Declare(
     doctest
     PREFIX ${CMAKE_CURRENT_BINARY_DIR}/doctest
     GIT_REPOSITORY https://github.com/doctest/doctest.git
-    GIT_TAG v2.4.12
+    GIT_TAG v2.5.3
 )
 
 
